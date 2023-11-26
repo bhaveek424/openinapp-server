dc5ab37e901c5232d9d7673a2e5b36579b552dc3
Bhaveek Jain <61945842+bhaveek424@users.noreply.github.com>
2023-11-26 20:42:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,3 +68,70 @@ app.get('/piechart', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+{/*
+long MOD = 998244353;
+
+        long[] ans = new long[N];
+
+        for (int i = 1; i <= N; i++) {
+            long sum = 0;
+            for (int j = L; j <= R; j++) {
+                long term = binomial(K - j * (i - 1) + i - 1, i - 1, MOD);
+                term = (term * binomial(j * N - K + i - 1, i - 1, MOD)) % MOD;
+                term = (term * power(j, N - i, MOD)) % MOD;
+                sum = (sum + term) % MOD;
+            }
+            ans[i - 1] = sum;
+        }
+
+        return ans;
+    }
+
+    static long binomial(long n, long k, long MOD) {
+        long res = 1;
+        for (long i = 0; i < k; ++i) {
+            res = (res * (n - i)) % MOD;
+            res = (res * modInverse(i + 1, MOD)) % MOD;
+        }
+        return res;
+    }
+
+    static long power(long base, long exp, long MOD) {
+        long result = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result = (result * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp = exp / 2;
+        }
+        return result;
+    }
+
+    static long modInverse(long a, long m) {
+        long m0 = m;
+        long t, q;
+        long x0 = 0, x1 = 1;
+
+        if (m == 1) {
+            return 0;
+        }
+
+        while (a > 1) {
+            q = a / m;
+            t = m;
+            m = a % m;
+            a = t;
+            t = x0;
+            x0 = x1 - q * x0;
+            x1 = t;
+        }
+
+        if (x1 < 0) {
+            x1 += m0;
+        }
+
+        return x1;
+    }
+*/}
